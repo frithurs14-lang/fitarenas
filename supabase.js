4da0b45470dbc07d8688786ed2d452eb3cf10ca6
FitLife <frithurs14@gmail.com>
2026-03-25 17:32:05 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,3 +3,5 @@ const SUPABASE_ANON_KEY = 'sb_publishable_8TYF85xqZbhGAS3gefR4rA_IA1indx-'
 
 const { createClient } = supabase
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+window._supabaseKey = 'sb_publishable_8TYF85xqZbhGAS3gefR4rA_IA1indx-'
